6923a5790d9cf8f51a03403f86f8379d62f3c444
kf3527 <kf3527@att.com>
2021-03-16 11:56:06 -0500

    finished step7

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t kf3527/udacity-ml-microservice .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -d --name ml-microservice -p 8000:80 kf3527/udacity-ml-microservice
